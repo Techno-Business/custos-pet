48f5a7c28580ae6af95c383f399a6c5f6f8f48c4
Lucas Gabriel <lucasogjc@gmail.com>
2022-10-13 17:17:33 -0300

    refactor: password confirmation attribute name

--- src/schemas/signup.schema.js
+++ src/schemas/signup.schema.js
@@ -3,10 +3,16 @@ import * as yup from 'yup';
 let signupSchema = yup.object().shape({
     firstName: yup.string().required('First name is required'),
     lastName: yup.string().required('Last name is required'),
-    email: yup.string().email('Enter a valid email address').required('Email address is required'),
-    password: yup.string().required('Password is required').min(8, 'Password must be at least 8 characters'),
-    passwordConfirm: yup.string().oneOf([yup.ref('password')], 'Passwords must be the same')
-    .required('Confirm password is required').min(8, 'Password must be at least 8 characters')
+    email: yup.string()
+        .email('Enter a valid email address')
+        .required('Email address is required'),
+    password: yup.string()
+        .required('Password is required')
+        .min(8, 'Password must be at least 8 characters'),
+    passwordConfirmation: yup.string()
+        .oneOf([yup.ref('password')], 'Passwords must be the same')
+        .required('Password confirmation is required')
+        .min(8, 'Password must be at least 8 characters')
 });
 
 export default signupSchema;
