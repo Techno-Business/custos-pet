7c588117fe77674dde2b752d4357f11a4b932ebb
Lucas Gabriel <lucasogjc@gmail.com>
2022-10-13 16:03:45 -0300

    feat: adds password length validation on signup schema

--- src/schemas/signup.schema.js
+++ src/schemas/signup.schema.js
@@ -4,9 +4,9 @@ let signupSchema = yup.object().shape({
     firstName: yup.string().required('First name is required'),
     lastName: yup.string().required('Last name is required'),
     email: yup.string().email('Enter a valid email address').required('Email address is required'),
-    password: yup.string().required('Password is required'),
+    password: yup.string().required('Password is required').min(8, 'Password must be at least 8 characters'),
     passwordConfirm: yup.string().oneOf([yup.ref('password')], 'Passwords must be the same')
-    .required('Confirm password is required')
+    .required('Confirm password is required').min(8, 'Password must be at least 8 characters')
 });
 
 export default signupSchema;
