859eef389b6d3ff8fd483004296fff1e58b54777
Lucas Gabriel <lucasogjc@gmail.com>
2022-10-13 18:16:37 -0300

    feat: adds password regex validation

--- src/schemas/signup.schema.js
+++ src/schemas/signup.schema.js
@@ -8,11 +8,12 @@ let signupSchema = yup.object().shape({
         .required('Email address is required'),
     password: yup.string()
         .required('Password is required')
-        .min(8, 'Password must be at least 8 characters'),
+        .min(8, 'Password must be at least 8 characters')
+        .matches(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,64}$/,
+            'Password must contain: number, lower/upper case letter and a special character'),
     passwordConfirmation: yup.string()
         .oneOf([yup.ref('password')], 'Passwords must be the same')
         .required('Password confirmation is required')
-        .min(8, 'Password must be at least 8 characters')
 });
 
 export default signupSchema;
