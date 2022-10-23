c9711283689ebdb8a2f6ffa51a65748f377cec50
Lucas Gabriel <lucasogjc@gmail.com>
2022-10-22 22:10:16 -0300

    fix: pet form category initial state

--- src/store/modules/app/reducer.js
+++ src/store/modules/app/reducer.js
@@ -4,7 +4,7 @@ import produce from 'immer';
 
 const INITIAL_STATE = {
     userForm: {},
-    petForm: {sex: 'Male', category: 'Other'},
+    petForm: {sex: 'Male', category: 'Dog'},
     costForm: {type: 'Service'},
     user: {},
     pet: {},
